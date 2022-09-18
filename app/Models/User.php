6f241973120a48fc923ef69a0c371cb1345dab40
مهند الشامي <49268942+MohanadAlShamey@users.noreply.github.com>
2022-09-18 11:48:53 +0300

    fix filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@admin.com') && $this->hasVerifiedEmail();
+        return $this->role=='admin' ;
     }
 }
