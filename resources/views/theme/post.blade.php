61558a9c19f773e19134f502906cf7912237fe7d
Mh_Shamey <mh.shamey@gmail.com>
2022-11-08 10:18:49 +0300

    fix image

--- resources/views/theme/post.blade.php
+++ resources/views/theme/post.blade.php
@@ -12,7 +12,7 @@
     <section class="border-bottom pb-4">
         <div class="container-fluid p-4">
             <div class="row">
-                <div class="col-md-12 col-lg-6">
+                <div class="col-md-12 ">
                     <div class="desc-video p-2">
                         <h3 class="text-uppercase font-weight-bold">{{$post->title}}</h3>
                         <p>{!! $post->info !!}</p>
